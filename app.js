06395f168f6963fd0be28f7fcd1ba8c185ff79e4
Sophie <sophtompa@gmail.com>
2025-10-08 13:14:13 +0100

    add get requests

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const cors = require('cors')
 const express = require('express');
 const app = express();
 const db = require("./db/connection.js")
+const endpoints = require("./endpoints.json")
+const { getEndpoints, getUsers, getEvents, getEventsByUser } = require('./controllers/events.controllers.js');
 
 app.use(cors());
 
 app.use(express.json());
+
+app.get("/api", getEndpoints);
+
+app.get("/api/users", getUsers)
+
+app.get("/api/events", getEvents)
+
+app.get("/api/events/:username", getEventsByUser)
+
+
 
 module.exports = app;
