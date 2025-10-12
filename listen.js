de513ba725c3be1a90b79859ccd7d69161f74128
Sophie <sophtompa@gmail.com>
2025-10-12 13:48:21 +0100

    small fix

--- listen.js
+++ listen.js
@@ -2,6 +2,18 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 9000;
 
+//Test
+const db = require('./db/connection');
+
+(async () => {
+  try {
+    const res = await db.query('SELECT 1');
+    console.log('Database connection works:', res.rows);
+  } catch (err) {
+    console.error('Database connection failed:', err);
+  }
+})();
+
 app.listen(PORT, (err) => {
     if(err) {
         console.log(err);
